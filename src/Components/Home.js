6099e4f9b22b54efcaf8f44a8cc9de8e7ee2f8be
mkbirb <mkbirb6520@googlemail.com>
2025-01-06 13:54:55 +1100

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <>
+            <p> Sunshine for</p>
+            <p> Sunshine for</p>
+            <p> Sunshine for</p>
+            <p> Sunshine for</p>
+            <p> Sunshine for</p>
+            <p> Sunshine for</p>
+        </>
+    );
+}
+
+export default Home;
